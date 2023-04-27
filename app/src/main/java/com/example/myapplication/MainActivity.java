95cd3fcda08e44dab0d068577e8990d63a67a854
dmswns0147@naver.com <dmswns0147@naver.com>
2023-04-27 18:22:43 +0900

    02.1: Activities and intents

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,34 +2,51 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private int mCount = 0;
-    private TextView mShowCount;
-
+    public static final int TEXT_REQUEST = 1;
+    private static final String LOG_TAG =
+            MainActivity.class.getSimpleName();
+    public static final String EXTRA_MESSAGE =
+            "com.example.myapplication.extra.MESSAGE";
+    private EditText mMessageEditText;
+    private TextView mReplyTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
         setContentView(R.layout.activity_main);
-        mShowCount = (TextView) findViewById(R.id.show_count);
+        mMessageEditText = findViewById(R.id.sample_EditText);
+        mReplyTextView = findViewById(R.id.text_message_reply);
+        onActivityResult(-1, 1, intent);
     }
 
-    public void showToast(View view) {
-        Toast toast = Toast.makeText(this, R.string.toast_message,
-                Toast.LENGTH_SHORT);
-        toast.show();
-    }
 
-    public void countUp(View view) {
-        mCount++;
-        if (mShowCount != null) {
-            mShowCount.setText(Integer.toString(mCount));
+    public void launchSecondActivity(View view) {
+        Intent intent = new Intent(this, activity_second.class);
+        String message = mMessageEditText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivityForResult(intent, TEXT_REQUEST);
+    }
+    @Override
+    public void onActivityResult(int requestCode,
+                                 int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == TEXT_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                String reply =
+                        data.getStringExtra(activity_second.EXTRA_REPLY);
+                mReplyTextView.setText(reply);
+                mReplyTextView.setVisibility(View.VISIBLE);
+            }
         }
     }
 }
